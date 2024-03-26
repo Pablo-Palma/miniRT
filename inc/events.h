5fc7bab4296248d12f1bbd3eff3705e8a2422e93
matheusbaeck <matheusbaeck@gmail.com>
2024-03-26 13:09:28 +0100

    add keys for linux, changes on gitignore

--- inc/events.h
+++ inc/events.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   events.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mamagalh@student.42madrid.com <mamagalh    +#+  +:+       +#+        */
+/*   By: math <math@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 10:27:32 by pabpalma          #+#    #+#             */
-/*   Updated: 2024/03/25 18:56:30 by mamagalh@st      ###   ########.fr       */
+/*   Updated: 2024/03/26 13:05:24 by math             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,25 @@
 
 #include "miniRT.h"
 
-# define KEY_ESC	53
-# define KEY_UP		126
-# define KEY_DOWN	125
-# define KEY_LEFT	123
-# define KEY_RIGHT	124
-# define KEY_W		13
-# define KEY_S		1
-# define KEY_A		0
-# define KEY_D		2
+// # define KEY_ESC	53
+// # define KEY_UP		126
+// # define KEY_DOWN	125
+// # define KEY_LEFT	123
+// # define KEY_RIGHT	124
+// # define KEY_W		13
+// # define KEY_S		1
+// # define KEY_A		0
+// # define KEY_D		2
+
+# define KEY_ESC    65307
+# define KEY_UP		65362
+# define KEY_DOWN	65364
+# define KEY_LEFT	65361
+# define KEY_RIGHT	65363
+# define KEY_W		119
+# define KEY_S		115
+# define KEY_A		97
+# define KEY_D		100
 
 
 #endif
