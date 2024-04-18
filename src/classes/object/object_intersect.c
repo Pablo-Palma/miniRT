88d7656eddba59f0574538cfb69ef97222240d6f
matheusbaeck <matheusbaeck@gmail.com>
2024-04-18 16:55:05 +0200

    changing intersect cyl

--- src/classes/object/object_intersect.c
+++ src/classes/object/object_intersect.c
@@ -51,26 +51,31 @@ static int intersect_sp(t_sphere sphere, t_ray *ray)
 	return 1;
 }
 
-static int	get_intersect_cy_caps(t_vec3 origin, t_vec3 dir, t_cyl cyl, double *t_cap)
+int	intersect_cyl_caps(t_vec3 origin, t_vec3 dir, t_cyl cyl, double *t_cap)
 {
 	t_vec3	cap_normal = cyl.dir;
 	double	cap_t[2] = {INFINITY, INFINITY};
 	int		hit = 0;
 	int		i = 0;
 	double	sign = 1.0;
+	*t_cap = INFINITY;
 
 	while(i < 2)
 	{
 		t_vec3	cap_center = vector_add(cyl.center, vector_scale(cap_normal, sign * (cyl.h / 2)));
-		double d = vector_dot_product(vector_sub(cap_center, origin), cap_normal) / vector_dot_product(dir, cap_normal);
-		if (d >= 0 && d < *t_cap)
+		double denominator = vector_dot_product(dir, cap_normal);
+		if (fabs(denominator) > EPSILON)
 		{
-			t_vec3	p = vector_add(origin, vector_scale(dir, d));
-			double	distance = vector_length(vector_sub(p, cap_center));
-			if (distance <= cyl.radius)
+			double d = vector_dot_product(vector_sub(cap_center, origin), cap_normal) / denominator;
+			if (d >= 0 && d < *t_cap)
 			{
-				cap_t[i] = d;
-				hit = 1;
+				t_vec3	p = vector_add(origin, vector_scale(dir, d));
+				double	distance = vector_length(vector_sub(p, cap_center));
+				if (distance <= cyl.radius + EPSILON)
+				{
+					cap_t[i] = d;
+					hit = 1;
+				}
 			}
 		}
 		i++;
@@ -85,74 +90,125 @@ static int	get_intersect_cy_caps(t_vec3 origin, t_vec3 dir, t_cyl cyl, double *t
 		*t_cap = closest_t;
 	}
 	return(hit);
+
+
 }
 
-static int	intersect_cyl(t_cyl cyl, t_ray *ray)
+int	intersect_ray_cyl(t_vec3 origin, t_vec3 dir, t_cyl cyl, double *t)
 {
-	t_vec3 diff = vector_sub(ray->origin, cyl.center);	//	vecot Origen-centro cilindro
-	t_vec3 dir_cross_cyl_dir = vector_cross(ray->direction, cyl.dir); //Cruz entre el rayo y el cilindro
+	t_vec3 diff = vector_sub(origin, cyl.center);	//	vecot Origen-centro cilindro
+	t_vec3 dir_cross_cyl_dir = vector_cross(dir, cyl.dir); //Cruz entre el rayo y el cilindro
 	t_vec3 diff_cross_cyl_dir = vector_cross(diff, cyl.dir); // Cruz entre diff y el cilindro
-	
+
 	double a = vector_dot_product(dir_cross_cyl_dir, dir_cross_cyl_dir); // Cuadrado del cruz de direcciones (rayo x cilindro)
 	double b = 2 * vector_dot_product(dir_cross_cyl_dir, diff_cross_cyl_dir); // 2 producto punto  ((cruz de rayo-cilindor) * (cruz de diff-cilindro))
 	double c = vector_dot_product(diff_cross_cyl_dir, diff_cross_cyl_dir) - (cyl.radius * cyl.radius); //cuadrado de (cruz de diff-cilindro) - (radio^2)
-	
+
 	double discriminant = b * b - 4 * a * c;
 	if (discriminant < 0)
 	    return 0; // No intersection
-	
+
 	double sqrt_discriminant = sqrt(discriminant);
 	double t0 = (-b - sqrt_discriminant) / (2 * a);		//Distancia desde el rayo al primer punto de interseccion
 	double t1 = (-b + sqrt_discriminant) / (2 * a);		//Distancia desde el rayo al segundo punto de interseccion
-	
+
 	double t_body = INFINITY;
 	int body_hit = 0;
 	//////******	t0	******///////
 	if (t0 >= EPSILON) //Evitar errores.
 	{
-	    double h0 = vector_dot_product(vector_add(diff, vector_scale(ray->direction, t0)), cyl.dir); // medida desde la base sobre el eje hasta el hitpoint
+	    double h0 = vector_dot_product(vector_add(diff, vector_scale(dir, t0)), cyl.dir); // medida desde la base sobre el eje hasta el hitpoint
 	    if (fabs(h0) <= cyl.h / 2) //Si está entre 0 y la altura máxima
 		{
 	        t_body = t0;	//SI Pertenece, almacenamos ese punto en t_body
 	        body_hit = 1;	//Pertenece
 	    }
 	}
-	
+
 	//////******	t1	******///////		t1 es un punto más lejano que t0
 	if (t1 >= EPSILON && t1 < t_body)	// si t1 es mayor que t_body, ya tenemos un punto más cercano, por tanto el lejano ya no nos interesa
 	{
-	    double h1 = vector_dot_product(vector_add(diff, vector_scale(ray->direction, t1)), cyl.dir);
+	    double h1 = vector_dot_product(vector_add(diff, vector_scale(dir, t1)), cyl.dir);
 	    if (fabs(h1) <= cyl.h / 2)
 		{
 			t_body = t1;
 			body_hit = 1;
 	    }
 	}
-	
-	// if (body_hit)
-	// 	ray->t = t_body;
 
 	// Intersección con las tapas del cilindro
 	double t_cap = INFINITY;
-	int cap_hit = get_intersect_cy_caps(ray->origin, ray->direction, cyl, &t_cap);
-	
+	int cap_hit = intersect_cyl_caps(origin, dir, cyl, &t_cap);
+
 	// calcular la intersección más cercana
 	if (cap_hit && (t_cap < t_body || !body_hit))
 	{
-		if (ray->t < t_cap)
-			return (0);
-		ray->t = t_cap;
+		*t = t_cap;
 		return 1;
 	}
 	else if (body_hit)
 	{
-		if (ray->t < body_hit)
-			return (0);
-		ray->t = t_body;
+		*t = t_body;
 		return 1;
 	}
 	return 0;
 }
+
+static int	intersect_cyl(t_cyl cyl, t_ray *ray)
+{
+	t_vec3	ray_origin = ray->origin;
+	t_vec3	ray_dir = ray->direction;
+	double	t = 0;
+	double	t_cap;
+	double	t_body;
+
+	int body_hit = intersect_ray_cyl(ray_origin, ray_dir, cyl, &t_body);
+	int cap_hit = intersect_cyl_caps(ray_origin, ray_dir, cyl, &t_cap);
+	if (body_hit || cap_hit)
+	{
+		if (body_hit && (!cap_hit || t_body < t_cap))
+            t = t_body;  // El cuerpo del cilindro es lo más cercano
+		else if (cap_hit)
+            t = t_cap;  // La tapa del cilindro es lo más cercano
+	}
+	if (t < ray->t)
+		return (1);
+	// if (t == t_cap)
+	// {
+	// 	t_vec3	hit_point = vector_add(ray_origin, vector_scale(ray_dir, t));
+	// 	t_vec3 diff_vector = vector_sub(hit_point, cyl.center);
+	// 	double dot_product = vector_dot_product(diff_vector, cyl.dir);
+	// 	cap_plane = (t_plane){.point = diff_vector, .normal = cyl.dir, .color = cyl.color};
+	// 	if (dot_product > 0)
+	// 		*ray->obj = (t_plane){.point = diff_vector, .normal = cyl.dir, .color = cyl.color};
+	// 	else
+	// 		*ray->obj = (t_plane){.point = diff_vector, .normal = vector_negate(cyl.dir), .color = cyl.color};
+	// }
+	return (0);
+}
+		// t_vec3	hit_point = vector_add(ray_origin, vector_scale(ray_dir, t));
+		// t_vec3	normal;
+		// else
+		// 	normal = cylinder_normal(hit_point, cyl);
+		// int	shadowed = shadow_plane(scene, hit_point);
+		// double	diffuse = 0;
+		// double	specular = 0;
+		// if (!shadowed)
+		// {
+		// 	t_vec3 light_dir = normalize(vector_sub(scene->light.pos, hit_point));
+		// 	diffuse = calculate_diffuse(light_dir, normal, scene->light.brigthness);
+		// 	t_vec3 view_dir = normalize(vector_sub(scene->cam.view_point, hit_point));
+    	// 	specular = calculate_specular(view_dir, light_dir, normal, 1.0, 10.0); // Intensidad y brillo arbitrarios
+		// }
+    	// int color = mix_colors(checker_color, diffuse, specular, *scene); // Ambient light contribution set to 0.1 arbitrarily
+    	// put_pixel_to_image(graph, x, y, color);
+		// if (color == 0)
+		// 	return(1);
+		// else
+    	// 	return (color); // Intersección encontrada
+	//}//
+// 	return 0; // No hubo intersección
+// }
 
 int intersect_light(t_light light, t_ray *ray)
 {
@@ -207,7 +263,8 @@ void intersect(t_obj *obj, t_ray *ray)
         ret = intersect_circle(*(t_circle *)(obj->child), ray);
 	if (ret)
 	{
-		*ray->obj = obj;
+		if (!*ray->obj)
+			*ray->obj = obj;
 		//printf("line:%s\n", ((t_obj *)(((t_list *)(*ray->obj))->content))->line);
 	}
 }
