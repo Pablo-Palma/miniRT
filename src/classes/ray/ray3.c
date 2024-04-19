2df00b17ab1f6182c2ee33293255e6268d1ddc3c
mamagalh <matheusbaeck@gmail.com>
2024-04-19 17:14:28 +0200

    raysum norminette

--- src/classes/ray/ray3.c
+++ src/classes/ray/ray3.c
@@ -6,13 +6,13 @@
 /*   By: mamagalh@student.42madrid.com <mamagalh    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 12:04:07 by mamagalh@st       #+#    #+#             */
-/*   Updated: 2024/04/19 12:04:18 by mamagalh@st      ###   ########.fr       */
+/*   Updated: 2024/04/19 17:14:04 by mamagalh@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-t_vec3	ray_sum(t_ray *ray, t_vec3 alight)
+typedef struct s_raysum
 {
 	t_list	*ray_list;
 	t_ray	*next_ray;
@@ -23,63 +23,76 @@ t_vec3	ray_sum(t_ray *ray, t_vec3 alight)
 	t_vec3	norm;
 	double	diffuse;
 	double	specular;
-	
-	pxl_light = (t_vec3){0,0,0};
-	ray_list = *ray->next;
-	while (ray_list)
+}			t_raysum;
+
+static void	handle_ray_to_light(t_raysum *rs, t_ray *ray, t_vec3 *alight)
+{
+	rs->temp = color_to_vec(*(*rs->next_ray->obj)->color);
+	rs->norm = normalize(get_normal(*ray->obj, rs->next_ray->origin));
+	rs->view_dir = normalize(ray->direction);
+	rs->light_dir = normalize(rs->next_ray->direction);
+	rs->diffuse = calculate_diffuse(rs->light_dir, rs->norm,
+			((t_light *)(*rs->next_ray->obj)->child)->brigthness);
+	rs->temp = vector_scale(rs->temp, rs->diffuse);
+	rs->temp = vector_fmax(rs->temp, *alight);
+	if (CHECKERBOARD == 0)
+		rs->temp = vector_multiply(rs->temp,
+				normalize(color_to_vec(*(*ray->obj)->color)));
+	else
+		rs->temp = vector_multiply(rs->temp,
+				normalize(color_to_vec(checkerboard(*ray->obj,
+							rs->next_ray->origin))));
+	rs->specular = calculate_specular(rs->view_dir, rs->light_dir, rs->norm,
+			1.0, 100.0);
+	rs->temp = vector_add(rs->temp,
+			vector_scale(color_to_vec(*(*rs->next_ray->obj)->color),
+				rs->specular));
+	rs->pxl_light = vector_add(rs->pxl_light, rs->temp);
+}
+
+static void	handle_ray_imgdir(t_raysum *rs, t_ray *ray, t_vec3 *alight)
+{
+	rs->temp = ray_sum(rs->next_ray, *alight);
+	rs->temp = vector_fmax(rs->temp, *alight);
+	if (CHECKERBOARD == 0)
+		rs->temp = vector_multiply(rs->temp,
+				normalize(color_to_vec(*(*ray->obj)->color)));
+	else
+		rs->temp = vector_multiply(rs->temp,
+				normalize(color_to_vec(checkerboard(*ray->obj,
+							rs->next_ray->origin))));
+	rs->pxl_light = rs->temp;
+}
+
+static void	handle_shadow(t_raysum *rs, t_ray *ray, t_vec3 *alight)
+{
+	if (CHECKERBOARD == 0)
+		rs->pxl_light = vector_multiply(*alight,
+				normalize(color_to_vec(*(*ray->obj)->color)));
+	else
+		rs->pxl_light = vector_multiply(*alight,
+				normalize(color_to_vec(checkerboard(*ray->obj,
+							rs->next_ray->origin))));
+}
+
+t_vec3	ray_sum(t_ray *ray, t_vec3 alight)
+{
+	t_raysum	rs;
+
+	rs.pxl_light = (t_vec3){0, 0, 0};
+	rs.ray_list = *ray->next;
+	while (rs.ray_list)
 	{
-		next_ray = ray_list->content;
-		if (!*next_ray->obj)
-			return (pxl_light);
-		if (is_child(*next_ray->obj, "L"))
-		{
-			temp = color_to_vec(*(*next_ray->obj)->color);
-			norm = normalize(get_normal(*ray->obj, next_ray->origin));
-			view_dir = normalize(ray->direction);
-			light_dir = normalize(next_ray->direction);
-			// if (vector_dot_product(norm, vector_sub(ray->origin, ray->direction)) < 0)
-			// 	norm = vector_negate(norm);
-			diffuse = calculate_diffuse(light_dir, norm, ((t_light *)(*next_ray->obj)->child)->brigthness);
-			temp = vector_scale(temp, diffuse);
-			temp = vector_fmax(temp, alight);
-			if (CHECKERBOARD == 0)
-				temp = vector_multiply(temp, normalize(color_to_vec(*(*ray->obj)->color)));
-			else
-				temp = vector_multiply(temp, normalize(color_to_vec(checkerboard(*ray->obj, next_ray->origin))));
-			specular = calculate_specular(view_dir, light_dir, norm, 1.0, 100.0);
-			temp = vector_add(temp, vector_scale(color_to_vec(*(*next_ray->obj)->color), specular));
-			pxl_light = vector_add(pxl_light, temp);
-		}
-		else if (next_ray->img_trace)
-		{
-			/*handle reflection*/
-			temp = ray_sum(next_ray, alight);
-			temp = vector_fmax(temp, alight);
-			if (CHECKERBOARD == 0)
-				temp = vector_multiply(temp, normalize(color_to_vec(*(*ray->obj)->color)));
-			else
-				temp = vector_multiply(temp, normalize(color_to_vec(checkerboard(*ray->obj, next_ray->origin))));
-			pxl_light = temp;
-			//pxl_light = vector_add(pxl_light, temp);
-		}
-		// else
-		// {
-		// 	temp = color_to_vec(BLACK);
-		// 	temp = vector_fmax(temp, alight);
-		// 	temp = vector_multiply(temp, normalize(color_to_vec(*(*ray->obj)->color)));
-		// 	temp = color_to_vec(BLACK);
-		// 	pxl_light = temp;
-		// }
-		ray_list = ray_list->next;
+		rs.next_ray = rs.ray_list->content;
+		if (!*rs.next_ray->obj)
+			return (rs.pxl_light);
+		if (is_child(*rs.next_ray->obj, "L"))
+			handle_ray_to_light(&rs, ray, &alight);
+		else if (rs.next_ray->img_trace)
+			handle_ray_imgdir(&rs, ray, &alight);
+		rs.ray_list = rs.ray_list->next;
 	}
-	if (vector_cmp(pxl_light, color_to_vec(BLACK)))
-	{
-		if (CHECKERBOARD == 0)
-			pxl_light = vector_multiply(alight, normalize(color_to_vec(*(*ray->obj)->color)));
-		else
-			pxl_light = vector_multiply(alight, normalize(color_to_vec(checkerboard(*ray->obj, next_ray->origin))));
-		// pxl_light = vector_fmax(pxl_light, alight);
-		// pxl_light = alight;
-	}
-	return (pxl_light);
+	if (vector_cmp(rs.pxl_light, color_to_vec(BLACK)))
+		handle_shadow(&rs, ray, &alight);
+	return (rs.pxl_light);
 }
