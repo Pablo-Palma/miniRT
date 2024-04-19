004024baeb09fa06613d5e758bece89935c9cae7
mamagalh <matheusbaeck@gmail.com>
2024-04-19 18:39:20 +0200

    norminette correction on render

--- src/render/render.c
+++ src/render/render.c
@@ -6,7 +6,7 @@
 /*   By: mamagalh@student.42madrid.com <mamagalh    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 18:14:03 by pabpalma          #+#    #+#             */
-/*   Updated: 2024/04/19 18:25:56 by mamagalh@st      ###   ########.fr       */
+/*   Updated: 2024/04/19 18:38:05 by mamagalh@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,47 +56,43 @@ void	ray_start(t_list *obj_list, t_list **pool, t_ray *ray)
 	}
 }
 
-static int	aux(t_list *obj_list, t_list **pool, t_list **ray_list,
-		t_ambient_light *alight)
+static void	first_intersect(t_list *obj_list, t_list **ray_list)
 {
-	t_list			*cur_ray;
-	int				color;
-
-	cur_ray = NULL;
-	color = BLACK;
-	ray_start(obj_list, pool, (*ray_list)->content);
-	cur_ray = *((t_ray *)((*ray_list)->content))->next;
-	if (*((t_ray *)cur_ray->content)->obj)
-		color = vec_to_color(ray_sum((*ray_list)->content,
-					vector_scale(color_to_vec((*alight).color),
-						(*alight).intensity)));
-	return (color);
-}
-
-static int	raytracing(t_list *obj_list, t_list **pool,
-		t_ray ray_dir, t_ambient_light *alight)
-{
-	t_list			**ray_list;
 	t_list			*cur;
-	int				color;
 
 	cur = NULL;
-	ray_list = lst_create();
-	ft_lstadd_back(ray_list, lst_getpool_node(pool, ray_new,
-			ray_cpy, &ray_dir));
 	cur = obj_list;
 	while (cur)
 	{
 		intersect(cur->content, (t_ray *)(*ray_list)->content);
 		cur = cur->next;
 	}
+}
+
+static int	raytracing(t_list *obj_list, t_list **pool,
+		t_ray ray_dir, t_ambient_light *alight)
+{
+	t_list			**ray_list;
+	t_list			*cur_ray;
+	int				color;
+
+	ray_list = lst_create();
+	ft_lstadd_back(ray_list, lst_getpool_node(pool, ray_new,
+			ray_cpy, &ray_dir));
+	first_intersect(obj_list, ray_list);
 	color = BLACK;
 	if (*((t_ray *)(*ray_list)->content)->obj
 		&& is_child(*((t_ray *)(*ray_list)->content)->obj, "L"))
 		color = *(((t_obj *)(*((t_ray *)(*ray_list)->content)->obj))->color);
 	else if (*((t_ray *)(*ray_list)->content)->obj)
 	{
-		aux(obj_list, pool, ray_list, alight);
+		cur_ray = NULL;
+		ray_start(obj_list, pool, (*ray_list)->content);
+		cur_ray = *((t_ray *)((*ray_list)->content))->next;
+		if (*((t_ray *)cur_ray->content)->obj)
+			color = vec_to_color(ray_sum((*ray_list)->content,
+						vector_scale(color_to_vec((*alight).color),
+							(*alight).intensity)));
 	}
 	ray_mv_to_pool(pool, ray_list);
 	return (color);
