9b47d39fab3d7dcdda920c55c83dafe67ead5492
mamagalh <matheusbaeck@gmail.com>
2024-04-19 11:44:31 +0200

    norminette OK checkerboard

--- src/classes/object/object_checkerboard.c
+++ src/classes/object/object_checkerboard.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   object_checkerboard.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: math <math@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mamagalh@student.42madrid.com <mamagalh    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 19:50:41 by mamagalh@st       #+#    #+#             */
-/*   Updated: 2024/04/19 00:26:15 by math             ###   ########.fr       */
+/*   Updated: 2024/04/19 11:38:21 by mamagalh@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,15 +56,6 @@ int	checkerboard_sp(t_sphere sphere, t_vec3 point)
 		return (BLACK);
 }
 
-static void	get_uv_cap(double *u, double *v, t_cyl cyl, t_vec3 point)
-{
-	t_vec3	local_point;
-
-	local_point = vector_sub(point, cyl.center);
-	*u = (local_point.x / cyl.radius + 1) / 2;
-	*v = (local_point.y / cyl.radius + 1) / 2;
-}
-
 static void	get_uv_cyl(double *u, double *v, t_cyl cyl, t_vec3 point)
 {
 	t_vec3	temp_vec;
@@ -72,6 +63,12 @@ static void	get_uv_cyl(double *u, double *v, t_cyl cyl, t_vec3 point)
 	double	height;
 
 	temp_vec = vector_sub(point, cyl.center);
+	if (cyl.caps != 0)
+	{
+		*u = (temp_vec.x / cyl.radius + 1) / 2;
+		*v = (temp_vec.y / cyl.radius + 1) / 2;
+		return ;
+	}
 	proj_length = vector_dot_product(temp_vec, cyl.dir);
 	temp_vec = vector_sub(temp_vec, vector_scale(cyl.dir, proj_length));
 	*u = (atan2(temp_vec.y, temp_vec.x) + M_PI) / (2 * M_PI);
@@ -88,10 +85,7 @@ int	checkerboard_cyl(t_cyl cyl, t_vec3 point)
 	double	u;
 	double	v;
 
-	if (cyl.caps == 1)
-		get_uv_cap(&u, &v, cyl, point);
-	else
-		get_uv_cyl(&u, &v, cyl, point);
+	get_uv_cyl(&u, &v, cyl, point);
 	x = (int)(floor(u * ((2 * M_PI * cyl.radius) / (cyl.radius / 2)))) % 2;
 	y = (int)(floor(v * (cyl.h / (cyl.radius / 2)))) % 2;
 	if (x == y)
